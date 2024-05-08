38c512fc248721b373704c5192759f38fffdcdb0
suash <suash.rb@gmail.com>
2024-05-08 22:56:01 +0545

    Nav position changed

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,19 @@
-let nav = document.getElementById('nav');
+let nav = document.querySelector('.nav');
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 1000) {
-        nav.style.display = '';
-        nav.style.transition = '2s ease-in-out' 
-    } else {
-        nav.style.display = 'none';
+
+
+    if (window.scrollY > 1050) {
+        nav.style.background = '#222222';
+        nav.style.transition = '5s ease-in-out' ;
+    }  else{
+        nav.style.background = 'linear-gradient(to bottom, #363737, #363636)';
+        nav.style.transition = '' ;
     }
 });
+
+let header = document.querySelector("header");
+
+// window.addEventListener("scroll", () => {
+//     header.classList.toggle('shadow', window.scrollY > 0);
+// })
