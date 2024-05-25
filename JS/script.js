f34aa02720c7453f36ddcdaec6dc90bc9a7f8641
suash <suash.rb@gmail.com>
2024-05-25 12:55:33 +0545

    Changes on name bar and navagation

--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,13 @@ window.addEventListener("scroll", () => {
 
 
     if (window.scrollY > 1050) {
-        nav.style.background = '#222222';
-        nav.style.transition = '5s ease-in-out' ;
+        // nav.style.background = '#222222';
+        nav.style.transition = '1s ease-in-out' ;
+        nav.style.boxShadow = '3px 3px 5px #313030';
     }  else{
         nav.style.background = 'linear-gradient(to bottom, #363737, #363636)';
+        nav.style.transition = '1s ease-in-out' ;
         nav.style.transition = '' ;
+        nav.style.boxShadow = '' ;
     }
 });
-
-let header = document.querySelector("header");
-
-// window.addEventListener("scroll", () => {
-//     header.classList.toggle('shadow', window.scrollY > 0);
-// })
